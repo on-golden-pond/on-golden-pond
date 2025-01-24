3544f1ff3bc1d3445d400abd9dc60b6777db2787
michael-masarik <masarikfamilymichael@gmail.com>
2025-01-24 12:07:09 -0600

    fixed bug

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ document.getElementById("copy_button").addEventListener("click", function() {
   const popoverElement = document.querySelector('[popover]');
   if (!HTMLDivElement.prototype.showPopover) {
     console.log("Popover API not supported on this device/browser.");
-    document.getElementById('submit-pic-button').style.bottom = '20px';
-    document.getElementById('leave-review').style.bottom = '20px';
+    document.querySelectorAll('.floating-button').forEach(function(button) {
+      button.style.bottom = '20px';
+    });
 } else {
     popoverElement.classList.remove('hidden');
     document.getElementById('contact').classList.remove('hidden');
