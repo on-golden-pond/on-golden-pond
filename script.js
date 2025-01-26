f7700e3906c3084b0c6b4e3e41598c78d8aa61e7
michael-masarik <masarikfamilymichael@gmail.com>
2025-01-26 10:12:39 -0600

    Added code for contact us page

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ document.getElementById("copy_button").addEventListener("click", function() {
       console.error('Error copying email to clipboard: ', err);
     });
   });
-//Popover Behavior for outdated hardware//
+// js for contact us page
+document.getElementById("copy_button-page").addEventListener("click", function() {
+  var email = document.getElementById("contact_email-page").innerText; // Get the email text
+  navigator.clipboard.writeText(email).then(function() {
+    alert("Email copied to clipboard!");
+  }, function(err) {
+    console.error('Error copying email to clipboard: ', err);
+  });
+});
+//Popover Behavior for outdated hardware
   const popoverElement = document.querySelector('[popover]');
   if (!HTMLDivElement.prototype.showPopover) {
     console.log("Popover API not supported on this device/browser.");
