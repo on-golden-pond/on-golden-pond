4cf1368f64d0ef59603696cc10d7dcfe912094e9
michael-masarik <masarikfamilymichael@gmail.com>
2025-01-26 15:51:53 -0600

    Added fallback button

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ document.getElementById("copy_button").addEventListener("click", function() {
 
 //Popover Behavior for outdated hardware
   const popoverElement = document.querySelector('[popover]');
+  const fallbackElement = document.getElementById('contact-fallback');
   if (!HTMLDivElement.prototype.showPopover) {
     console.log("Popover API not supported on this device/browser.");
     document.querySelectorAll('.floating-button').forEach(function(button) {
       button.style.bottom = '20px';
     });
+    fallbackElement.classList.remove('hidden');
 } else {
     popoverElement.classList.remove('hidden');
     document.getElementById('contact').classList.remove('hidden');
