c86b7ffb09bab66b86ce0310bbda4e23293bd44a
michael-masarik <masarikfamilymichael@gmail.com>
2025-01-26 15:42:06 -0600

    fixed bug

--- script.js
+++ script.js
@@ -7,15 +7,7 @@ document.getElementById("copy_button").addEventListener("click", function() {
       console.error('Error copying email to clipboard: ', err);
     });
   });
-// js for contact us page
-document.getElementById("copy_button-page").addEventListener("click", function() {
-  var email = document.getElementById("contact_email-page").innerText; // Get the email text
-  navigator.clipboard.writeText(email).then(function() {
-    alert("Email copied to clipboard!");
-  }, function(err) {
-    console.error('Error copying email to clipboard: ', err);
-  });
-});
+
 //Popover Behavior for outdated hardware
   const popoverElement = document.querySelector('[popover]');
   if (!HTMLDivElement.prototype.showPopover) {
